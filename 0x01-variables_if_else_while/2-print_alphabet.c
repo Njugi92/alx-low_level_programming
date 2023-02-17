e41364c0f68117f894d57763f6e2cc319b72aa13
karuma <kenkaruma.kk@gmail.com>
2023-02-17 22:56:48 +0300

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets in lower case
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++);
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
