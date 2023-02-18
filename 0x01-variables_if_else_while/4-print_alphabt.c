46729f5452e7ac22d7106d546685e62432e1b189
karuma <kenkaruma.kk@gmail.com>
2023-02-17 18:37:57 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
-
 /**
- * main - print alphabets in lower case followed by new line, except 'e' and 'q'
+ * main - Entry
  * Return: 0 (success)
  */
 int main(void)
 {
 	char la;
-	
+
 	for (la = 'a'; la <= 'z'; la++)
-	{
-		if (la != 'e' && la != 'q')
-			putchar(la);
-	
+       	{
+	if (la != 'e' && la != 'q')
+	putchar(la);
 	}
-
 	putchar('\n');
-
 	return (0);
-}
+	}
