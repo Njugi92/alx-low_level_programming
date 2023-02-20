b6c49736c7829e24c8dd4e0fe1776020dd286483
karuma <kenkaruma.kk@gmail.com>
2023-02-19 16:05:53 -0800

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - prints all base 10 numbers
+ * main - Entry point
  * Return: 0 (success)
  */
 int main(void)
 {
 	int x;
 
-	for (x = 0; x < 10; x++)
-	printf("%d", x);
+	x = 0;
+	while (x < 10)
+	{
+		putchar(x + '0');
+		x++;
+	}
 	putchar('\n');
 	return (0);
 }
